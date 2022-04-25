ec1e8b398813d2095ddee54c33e84256e107bf90
Jeremy Ansel <jeremyansel@hotmail.com>
2022-04-26 00:15:11 +0300

    Updated room form to fit design

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
+import django.forms as forms
 from .models import Room
 
 class RoomForm(ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'id':"room_name_input", 'name':"name", 'class': 'form-control'}))
+    topic = forms.Select(attrs={'id':"room_topic_input", 'name':"topic", 'class': 'form-control'})
     class Meta:
         model = Room
-        exclude = ['host']
+        exclude = ['host', 'participants']
