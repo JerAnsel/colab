9bb024ee3901967757a70ca8d859fcb44f87df51
Jeremy Ansel <jeremyansel@hotmail.com>
2022-04-27 13:27:16 +0300

    Added and registered profile and languages models

--- base/models.py
+++ base/models.py
@@ -1,8 +1,36 @@
-from typing import ClassVar
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class CodingLanguage(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class SpokenLanguage(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    coding_languages = models.ManyToManyField(CodingLanguage, related_name='coding_languages', blank=True)
+    spoken_languages = models.ManyToManyField(SpokenLanguage, related_name='spoken_languages', blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -15,7 +43,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
