beafc0425740eb571488f482e1f07b85cc58aa47
Jeremy Ansel <jeremyansel@hotmail.com>
2022-06-09 15:42:53 +0300

    Added profile page

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings 
+from django.conf.urls.static import static  
 from . import views
 
 urlpatterns = [
@@ -11,5 +13,11 @@ urlpatterns = [
 
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('profile-page/<str:pk>/', views.profile, name="profile-page"),
+    path('test', views.test, name='test'),
+    path('images', views.images, name='images'),
     # path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
 ]
+
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
--- base/views.py
+++ base/views.py
@@ -5,9 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Message, Room, Topic
-from .forms import RoomForm
+from .models import Message, Room, Topic, UploadImage
+from .forms import RoomForm, UserImage, UserForm, ProfileForm
 
 # Create your views here.
 
@@ -20,6 +19,7 @@ from .forms import RoomForm
 def loginPage(request):
 
     page = 'login'
+    user_form = UserForm()
     topics = Topic.objects.all()
 
     if request.user.is_authenticated:
@@ -42,7 +42,7 @@ def loginPage(request):
         else:
             messages.error(request, 'Username OR password does not exist')
 
-    context = {'page': page, 'topics': topics}
+    context = {'page': page, 'topics': topics, 'user_form': user_form}
     return render(request, 'base/login_register.html', context)
     
 def logoutUser(request):
@@ -50,21 +50,42 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    user_form = UserForm()
+    #profile_form = ProfileForm()
     topics = Topic.objects.all()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
+        user_form = UserForm(request.POST)
+        # profile_form = ProfileForm(request.POST)
+        if user_form.is_valid(): #and profile_form.is_valid()
+            user = user_form.save(commit=False)
             user.username = user.username.lower()
             user.save()
             login(request, user)
             return redirect('home')
+
+            # Code to render home page after register.
+            # q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+            # rooms = Room.objects.filter(
+            # Q(topic__name__icontains=q) |
+            # Q(name__icontains=q) |
+            # Q(description__icontains=q) 
+            # )
+            # topics = Topic.objects.all()
+            # room_count = rooms.count()
+
+            # context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'registered': True}
+
+            # return render(request, 'base/home.html', context)
         else:
             messages.error(request, 'An error occured during registration')
 
-    return render(request, 'base/login_register.html', {'form': form, 'topics': topics})
+    return render(request, 'base/login_register.html', {'user_form': user_form, 'topics': topics})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -171,3 +192,62 @@ def updateRoom(request, pk):
 #         room.delete()
 #         return redirect('home')
 #     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='/login')
+def profile(request, pk):
+    topics = Topic.objects.all()
+
+    if not request.user.id == int(pk):
+            return redirect('home')
+    
+    profile_form = ProfileForm()
+    user_form = UserForm()
+    update = False
+
+    if request.method == 'POST':
+        user = request.user
+        data = dict(zip(request.POST.keys(), request.POST.values()))
+        data['user'] = user
+        first_name = request.POST.get('fullName').split()[0]
+        last_name = request.POST.get('fullName').split()[1]
+        user_data = {'username':user.username,
+                     'first_name': first_name,
+                     'last_name': last_name,
+                     'email': user.email,
+                     'password': user.password,
+                     'password2': user.password}
+        data.update(user_data)
+
+        profile_form = ProfileForm(instance=user.profile, data=data)
+        user_form = UserForm(instance=user, data=data)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            return redirect('home')
+
+    context = {'topics': topics, 'user_form': user_form, 'profile_form': profile_form, 'update': update}
+
+
+    prof = request.user.profile
+
+    context = {'topics': topics, 'profile': prof}
+    return render(request, 'base/profile_page.html', context)
+
+def test(request):
+    if request.method == 'POST':  
+        form = UserImage(request.POST, request.FILES)  
+        if form.is_valid():  
+            form.save()  
+  
+            # Getting the current instance object to display in the template  
+            img_object = form.instance  
+              
+            return render(request, 'base/test.html', {'form': form, 'img_obj': img_object})  
+    else:  
+        form = UserImage()
+        form2 = UserForm()  
+   
+    return render(request, 'base/test.html', {'form': form, 'form2': form2})
+
+def images(request):
+    images = UploadImage.objects.all()
+    return render(request, 'base/images.html', {'images': images})
