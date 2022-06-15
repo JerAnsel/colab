8ce59c2e3cbb1eb498116b9dfeeac48ac4bd8e23
Jeremy Ansel <jeremyansel@hotmail.com>
2022-06-15 22:27:00 +0300

    Updated settings, put django-heroku last

--- studybud/settings.py
+++ studybud/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,3 +140,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
